80c8e5401e06d8823e60ebc46c4d29631f6d0d68
danielgolabek <daniel.golabek@gmail.com>
2018-04-01 22:15:07 +0100

    SshClient tests

---
+++ BlendedJS.Tests/Ssh/SshClientTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlendedJS.Tests.Ssh
+{
+    [TestClass]
+    public class SshClientTests
+    {
+        [TestMethod]
+        public void Tunnel()
+        {
+            BlendedJSEngine engine = new BlendedJSEngine();
+            var result = engine.ExecuteScript(
+                @"
+                    var sshClient = new SshClient({host:'',user:'', privateKey:''});
+                    sshClient.tunnel();
+                ");
+            Assert.IsNull(result.Value);
+        }
+    }
+}
