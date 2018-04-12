370362550ba0ffa870ac9917214e334d2041b7d7
danielgolabek <daniel.golabek@gmail.com>
2018-04-12 19:44:33 +0100

    ValueType

--- BlendedJS/Types/ValueType.cs
+++ BlendedJS/Types/ValueType.cs
@@ -3,5 +3,10 @@
     public abstract class ValueType
     {
         public abstract object GetValue();
+
+        public override string ToString()
+        {
+            return GetValue().ToStringOrDefault();
+        }
     }
 }
